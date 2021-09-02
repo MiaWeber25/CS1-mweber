87e00be74bfec2912a1a334f689757662492dd02
Mia Weber <miaweber02@gmail.com>
2021-09-02 13:46:44 -0600

    day1 progress on ASCIIArt Lab

--- labs/ascii/ASCIIArt.cpp
+++ labs/ascii/ASCIIArt.cpp
@@ -20,21 +20,31 @@ using namespace std; //resolve cout, cin, and endl names
 //main entry point of the program
 int main()
 {
-    //FIXME3: declare a variable to store name
-    //FIXME4: prompt user to enter their name and store the value in variable
+    string userName;
+    cout << "enter your name: ";
+    cin >> userName; //getline(cin, userName);
+    cout << "Nice to meet you, " << userName + "!\n";
+    //FIXME3: declare a variable to store name FIXED 09/02
+    //FIXME4: prompt user to enter their name and store the value in variable FIXED 09/02
 
     //FIXME5: greet the name using the variable as the following output
-    //must output: Nice meeting you, <name>!
+    //must output: Nice meeting you, <name>! 
 
-    cout << "Hope you like my Tom & Jerry ASCII art...\n\n";
+    cout << "\nHope you like my Tom & Jerry ASCII art...\n\n";
 
-    string line1 = " |\\_/|  *****************************  (\\_/)\n";
+    string line1 = " |\\_/|   *****************************    (\\_/)\n";
     cout << line1;
 
     //FIXME6: use variable to print the second line of the graphic
+    string line2 = "/ @ @ /  *       ASCII Lab           *   (='.'=)";
+    cout << line2;
     //FIXME7: print the third line of the graphics
+    //string line3 = "( > 0 < )  *     By: Mia Weber      *   (")_(")";
+    //cout << line3;
     //FIXME8: use variable to print the fourth line
+    string line4 = "";
     //FIXME9: use variable to print the fifth line
+    string line5 = "";
     //NOTE: You can add more lines or print more ASCII arts of your choice if you'd like...
 
     cout <<"\nGoodbye... hit enter to exit the program: " << '\n';

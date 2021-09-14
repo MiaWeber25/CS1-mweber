71e4ab5bec900fe21ebdda5ec9cbded2d9b8f1d0
Mia Weber <miaweber02@gmail.com>
2021-09-14 14:41:12 -0600

    HW2 FINAL SUBMISSION

--- assignments/triangle/main.cpp
+++ assignments/triangle/main.cpp
@@ -7,7 +7,7 @@
     This program also verifies that the given side lengths will make a triangle. 
 
     Useful resource for functions: https://www.tutorialspoint.com/cplusplus/cpp_functions.htm 
-    Useful resource for nan(Not a Number): https://stackoverflow.com/questions/23521834/c-basics-if-statement-testing 
+    Useful resource for nan(Not a Number): https://www.tutorialspoint.com/checking-if-a-double-or-float-is-nan-in-cplusplus  
    
     Algorithm steps:
     1. prompt the user to enter three sides of a triangle
@@ -24,7 +24,7 @@ using namespace std;
 //perimeter function - called to calculate perimeter of triangle
 float perimeter(float len1, float len2, float len3) {
     float result = len1 + len2 + len3; //math that outputs the perimeter of the triangle 
-    cout << "The perimeter of your triangle is: " << result << endl; //print the perimeter 
+    cout << "\nThe perimeter of your triangle is: " << result << endl; //print the perimeter 
     return (0);
 }
 
@@ -32,20 +32,25 @@ float perimeter(float len1, float len2, float len3) {
 float area(float len1, float len2, float len3) {
     float s = (len1+len2+len3)/2; //calculate the semiperimiter 
     float result = sqrt(s*(s-len1)*(s-len2)*(s-len3)); //math that outputs the area of the triangle
-    cout << "The area of your triangle is: " << result << endl; //print the area
-     
+    cout << "\nThe area of your triangle is: " << result << endl; //print the area
+
+//PROBLEM: if result = 0 it will go through if statement. if result = NaN it won't even enter logic statement...
+//FIXED: seperated isan() and result == 0 into if and else if.
+
     //if else statement to notify user if triangle is correct
-    if (result == 0 | result == nan("")) { //check if area = 0 or if area = non-real number (neg. sqrt)
-        cout<< "You have failed to correctly make a triangle!\n";
+    if (isnan(result)) {
+        cout<< "\nYou have failed to correctly make a triangle!\n";
+    } else if (result == 0) {
+        cout<< "\nYou have failed to correctly make a triangle!\n";
     } else if (result >=0.1) {
-        cout<< "You have correctly made a triangle.\n";
+        cout<< "\nYou have correctly made a triangle.\n";
     }
     return (0);
 }
 
 //main function - take user input and print output of functions above
 int main() {
-    //local variable declaration: 
+    //local variable declaration
     float length1;
     float length2;
     float length3;
@@ -64,12 +69,11 @@ int main() {
     cout << "Please enter the length of side 3 of your triangle: \n";
     cin >> length3; //store user input into length3
 
-    //CALL AREA FUNCTION
-    area(length1, length2, length3);
-    
-
     //CALL PERIMETER FUNCTION
    perimeter(length1, length2, length3);
+   
+    //CALL AREA FUNCTION
+    area(length1, length2, length3);
     
     return 0;
 }

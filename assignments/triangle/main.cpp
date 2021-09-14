c71036202759bbe782a2fc3c6557aa613d06e164
Mia Weber <miaweber02@gmail.com>
2021-09-14 13:17:31 -0600

    updated main.cpp

--- assignments/triangle/main.cpp
+++ assignments/triangle/main.cpp
@@ -7,12 +7,12 @@
     This program also verifies that the given side lengths will make a triangle. 
 
     Useful resource for functions: https://www.tutorialspoint.com/cplusplus/cpp_functions.htm 
-    Useful resource for nan: https://stackoverflow.com/questions/23521834/c-basics-if-statement-testing 
+    Useful resource for nan(Not a Number): https://stackoverflow.com/questions/23521834/c-basics-if-statement-testing 
    
     Algorithm steps:
     1. prompt the user to enter three sides of a triangle
-    2. calculate and print the area of a triangle using Heron's formula
-    3. calculate and print the perimeter of the given triangle
+    2. calculate and print the perimeter of the given triangle
+    3. calculate and print the area of a triangle using Heron's formula 
     4. BONUS: test if 3 sides actually form a triangle. 
 */
 
@@ -21,21 +21,21 @@
 #include <math.h>
 
 using namespace std;
-
+//perimeter function - called to calculate perimeter of triangle
 float perimeter(float len1, float len2, float len3) {
     float result = len1 + len2 + len3; //math that outputs the perimeter of the triangle 
-    cout << "The perimeter of your triangle is: " << result << endl;
+    cout << "The perimeter of your triangle is: " << result << endl; //print the perimeter 
     return (0);
 }
 
-//area function - called to calculate area
+//area function - called to calculate area of triangle
 float area(float len1, float len2, float len3) {
-    float s = (len1+len2+len3)/2; //semiperimiter 
+    float s = (len1+len2+len3)/2; //calculate the semiperimiter 
     float result = sqrt(s*(s-len1)*(s-len2)*(s-len3)); //math that outputs the area of the triangle
-    cout << "The area of your triangle is: " << result << endl;
+    cout << "The area of your triangle is: " << result << endl; //print the area
      
     //if else statement to notify user if triangle is correct
-    if (result == 0 | result == nan("")) { //if (isnan(result))
+    if (result == 0 | result == nan("")) { //check if area = 0 or if area = non-real number (neg. sqrt)
         cout<< "You have failed to correctly make a triangle!\n";
     } else if (result >=0.1) {
         cout<< "You have correctly made a triangle.\n";
@@ -50,29 +50,27 @@ int main() {
     float length2;
     float length3;
 
-
     //SIDE LENGTH ONE
     cout << "Please enter the lenth of side 1 of your triangle: \n";
-    cin >> length1;
-    cin.ignore(256,'\n');
+    cin >> length1; //store user input into length1
+    cin.ignore(256,'\n'); //use cin.ignore to ignore the extra space between calling cins (ex: I call cin again below)
 
     //SIDE LENGTH TWO 
     cout << "Please enter the length of side 2 of your triangle: \n";
-    cin >> length2;
+    cin >> length2; //store user input into length2
     cin.ignore(256,'\n');
 
     //SIDE LENGTH THREE
     cout << "Please enter the length of side 3 of your triangle: \n";
-    cin >> length3;
+    cin >> length3; //store user input into length3
 
-    //CALCULATE AREA
+    //CALL AREA FUNCTION
     area(length1, length2, length3);
     
 
-    //CALCULATE PERIMETER
+    //CALL PERIMETER FUNCTION
    perimeter(length1, length2, length3);
     
-
     return 0;
 }
 

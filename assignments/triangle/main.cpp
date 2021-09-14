85dd156c742506a5ec84a4db3d9690dc9fb9901f
Mia Weber <miaweber02@gmail.com>
2021-09-14 13:05:41 -0600

    progress on main.cpp

--- assignments/triangle/main.cpp
+++ assignments/triangle/main.cpp
@@ -7,6 +7,7 @@
     This program also verifies that the given side lengths will make a triangle. 
 
     Useful resource for functions: https://www.tutorialspoint.com/cplusplus/cpp_functions.htm 
+    Useful resource for nan: https://stackoverflow.com/questions/23521834/c-basics-if-statement-testing 
    
     Algorithm steps:
     1. prompt the user to enter three sides of a triangle
@@ -17,30 +18,30 @@
 
 #include <iostream>
 #include <string>
+#include <math.h>
 
 using namespace std;
 
-//area function - called to calculate area
-float area(float len1, float len2, float len3) {
-    float result;
-    //result = //math that outputs the area of the triangle
-    cout << "The area of your triangle is: " << result << endl;
-    return (0); 
-}
-
 float perimeter(float len1, float len2, float len3) {
-    float result;
-    result = len1 + len2 + len3; //math that outputs the perimeter of the triangle 
+    float result = len1 + len2 + len3; //math that outputs the perimeter of the triangle 
     cout << "The perimeter of your triangle is: " << result << endl;
     return (0);
 }
 
-//perimeter function - called to calculate perimeter
-    //int perimeter(float length1, float length2, float length3) {
-        //float result;
-       // result = length1 + length2 + length3; //math that outputs the perimeter of the triangle 
-       // return (result);
-    //}
+//area function - called to calculate area
+float area(float len1, float len2, float len3) {
+    float s = (len1+len2+len3)/2; //semiperimiter 
+    float result = sqrt(s*(s-len1)*(s-len2)*(s-len3)); //math that outputs the area of the triangle
+    cout << "The area of your triangle is: " << result << endl;
+     
+    //if else statement to notify user if triangle is correct
+    if (result == 0 | result == nan("")) { //if (isnan(result))
+        cout<< "You have failed to correctly make a triangle!\n";
+    } else if (result >=0.1) {
+        cout<< "You have correctly made a triangle.\n";
+    }
+    return (0);
+}
 
 //main function - take user input and print output of functions above
 int main() {
@@ -59,7 +60,7 @@ int main() {
     cout << "Please enter the length of side 2 of your triangle: \n";
     cin >> length2;
     cin.ignore(256,'\n');
-    
+
     //SIDE LENGTH THREE
     cout << "Please enter the length of side 3 of your triangle: \n";
     cin >> length3;
@@ -72,13 +73,6 @@ int main() {
    perimeter(length1, length2, length3);
     
 
-    //if else statement to notify user if triangle is correct
-    //NOT 180 = --> USE THE PATHAGORIAN THEREUM
-    if (length1 + length2 + length3 == 180) {
-        cout<< "You have correctly made a triangle!";
-    } else {
-        cout<< "You have failed to correctly make a triangle.";
-    }
-
     return 0;
 }
+

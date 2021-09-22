dc9cd4b3fbd09203fe0d14513605cae3e69c8c86
Mia Weber <miaweber02@gmail.com>
2021-09-21 20:36:04 -0600

    updated main.cpp

--- labs/functions/main.cpp
+++ labs/functions/main.cpp
@@ -24,7 +24,7 @@ Algorithm steps:
 
 using namespace std;
 
-//setprecision()????
+//constant to ensure answer is within margin or error
 const float epsilon = 1e-5; //0.00001 accurary up to 5 decimal points; margin of error
 
 //function prototypes
@@ -34,6 +34,8 @@ double findDistance(int, int, int, int); //WHY DO I NEED THIS UP HERE???????
 
 //test function that runs automated testing
 void test();
+
+void clearScreen();
 
 //function to clear the screen
 //NOTE: system call is not a security best practice!
@@ -59,21 +61,21 @@ int main() {
         cout << "Enter a point in the form (x,y): ";
         
         //parse the input stream
-        cin >> x1 >> ch >> y1; //CHANGED SO THAT THERE'S JUST ONE CHAR IGNORED (,)
+        cin >> ch >> x1 >> ch >> y1 >> ch; //CHANGED SO THAT THERE'S JUST ONE CHAR IGNORED (,)
         printf("(x1,y1) = (%d, %d)\n",x1,y1);
 
         cout << "Enter a second point in the form (x,y): ";
         
         //FIXME 3 - read/parse the second point and store data into variablesl x2 and y2 #FIXED#
-        cin >> x2 >> ch >> y2;
+        cin >> ch >> x2 >> ch >> y2 >> ch;
         printf("(x2,y2) = (%d, %d)\n",x2,y2);
         
         //FIXME 4 - call test function #ATTEMPTED FIX#
-        /////////////////test(); 
+        //////test(); 
         //FIXME5 - call findDistance() passing proper arguments #ATTEMPTED FIX#
-        findDistance(x1,y1,x2,y2);
+        double calculated_distance = findDistance(x1,y1,x2,y2);
         //FIXME6 - using printf function display the returned distance with proper description #ATTEMPTED FIX, but add variables in# 
-        
+        printf("The distance between those points is: %lf\n",calculated_distance);
     }
 
     cin.ignore(1000, '\n');
@@ -85,16 +87,28 @@ int main() {
 
 double findDistance(int x1, int y1, int x2, int y2) {
     //FIXME7 - find the distance between (x1, y1) and (x2, y2) #FIXED#
-    double distance = sqrt(((x2-x1)^2) + ((y2-y1)^2));
-    printf("The distance between those points is: %lf\n",distance); 
-    return 0.000000;
+    double distance = sqrt(pow(x2-x1,2) + pow(y2-y1,2)); 
+    return distance;
 }
 
 //test function that tests findDistance function with 3 test cases
+//use a loop to loop through an array of test case data? 
 void test() {
     float result = findDistance(4,3,5,1);
     float expected = 2.236067f;
-    assert( fabs(result-expected) <= epsilon); //accept the result if it's less than the error of margin
+    assert(fabs(result-expected) <= epsilon); //accept the result if it's less than the error of margin
     //FIXME8 - add at least two mores test cases
+    result = findDistance(-4,3,5,1);
+    expected = 0;
+    assert(fabs(result-expected) <= epsilon);
+   
+    result = findDistance(4,-3,5,1);
+    expected = 0;
+    assert(fabs(result-expected) <= epsilon);
+
+    result = findDistance(-4,-3,-5,-1);
+    expected = 0;
+    assert(fabs(result-expected) <= epsilon);
+
     cerr << "all tests passed..." <<endl;
 }

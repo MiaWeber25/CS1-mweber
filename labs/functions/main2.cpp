fd3ee201525603d3a38b424950f7e1a61cf7b1c2
Mia Weber <miaweber02@gmail.com>
2021-09-22 00:11:28 -0600

    added comments

--- labs/functions/main2.cpp
+++ labs/functions/main2.cpp
@@ -24,17 +24,17 @@ Algorithm steps:
 
 using namespace std;
 
-//constant to ensure answer is within margin or error
-const float epsilon = 1e-5; //0.00001 accurary up to 5 decimal points; margin of error
+//constant to ensure answer is within margin or error (0.00001 accuracy up to 5 decimal points)
+const float epsilon = 1e-5; 
 
 //FUNCTION PROTOTYPES
-//function that calculates the distance between two points (x1,y1) & (x2,y2)
-double findDistance(int, int, int, int); //WHY DO I NEED THIS UP HERE??????? 
-//function that runs automated testing
-void test();
-//function that clears console
-void clearScreen();
-bool want_continue(bool);
+double findDistance(int, int, int, int); //function that calculates distance between (x1,y1) and (x2,y2)
+
+void test(); //function that runs automated testing
+
+void clearScreen(); //function that clears console
+
+bool want_continue(bool); //function that checks if the user wants to enter more points
 
 //NOTE: system call is not a security best practice!
 void clearScreen() {
@@ -48,39 +48,39 @@ void clearScreen() {
 
 bool want_continue() {
     char response;
-    bool repeat = false;
-    //bool repeat1 = 0;
-    cout << "Would you like to enter more points? Y/N\n";
+    bool repeat = false; //set to false automatically to avoid infinite loop
+    
+    cout << "Would you like to enter more points? Y/N\n"; //ask if user wants to enter more points
     cin >> response;
-    cin.ignore(1000, '\n');
+    cin.ignore(1000, '\n'); //ignore white space after cin
 
-    if (response == 'Y') {
+    if (response == 'Y') { //if the user enters 'Y' then set repeat to true (used in while loop in main)
         repeat = true;
         return repeat;
-        //repeat1 = true;
     } 
-    else if (response == 'N') {
+    else if (response == 'N') { //if the user enters 'N' then set repeat to false (therefore skipping the while loop)
         repeat = false;
-        cout << "Enter to quit the program: ";
+        cout << "Enter to quit the program: "; //allows the user to exit the program without entering more points
         cin.get();
         cout << "Goodbye..." << endl;
         return repeat;
     }
-    //return repeat1; 
-    //return repeat; THIS ALLOWS THE FUNCTION TO RETURN SOMETHING ALWAYS??????
 }
-double findDistance(int x1, int y1, int x2, int y2) {
+
+double findDistance(int x1, int y1, int x2, int y2) { //function that actually does the math to find the distance
     //FIXME7 - find the distance between (x1, y1) and (x2, y2) #FIXED#
     double distance = sqrt(pow(x2-x1,2) + pow(y2-y1,2)); 
     return distance;
     }
-        //test function that tests findDistance function with 3 test cases
-        //use a loop to loop through an array of test case data? 
+
+//test function that tests findDistance function with 3 test cases
+//use a loop to iterate through an array of test case data? (i++)
 void test() {
     float result = findDistance(4,3,5,1);
     float expected = 2.236067f;
     assert(fabs(result-expected) <= epsilon); //accept the result if it's less than the error of margin
-    //FIXME8 - add at least two mores test cases
+    //FIXME8 - add at least two mores test cases #FIXED#
+
     result = findDistance(-4,3,5,1);
     expected = 9.219544;
     assert(fabs(result-expected) <= epsilon);
@@ -97,18 +97,18 @@ void test() {
     }
 
 int main() {
-   bool repeat = true;
+   bool repeat = true; //initialize repeat to true in order to enter while loop
     int x1, y1, x2, y2; //vars to store two points (x1,y1) and (x2,y2)
     char ch; 
 
-    //FIXME bonus - add loop until user wants to quit
-    while (repeat == true) {
+    //FIXME bonus - add loop until user wants to quit #FIXED#
+    while (repeat == true) { //while the user wants to repeat, prompt for points and calculate distance between them
         clearScreen();
         cout << "Program calculates distance between 2 points on a 2D coordinate." <<endl;
         cout << "Enter a point in the form (x,y): ";
 
         //parse the input stream
-        cin >> ch >> x1 >> ch >> y1 >> ch; //CHANGED SO THAT THERE'S JUST ONE CHAR IGNORED (,)
+        cin >> ch >> x1 >> ch >> y1 >> ch; //take the values entered and ignore (,)
         printf("(x1,y1) = (%d, %d)\n",x1,y1);
 
         cout << "Enter a second point in the form (x,y): ";
@@ -126,17 +126,9 @@ int main() {
         //FIXME6 - using printf function display the returned distance with proper description #FIXED# 
         printf("The distance between those points is: %lf\n",calculated_distance);
         
-    //FIX THE ORDER TO EXIT THE PROGRAM AND ASK TO ENTER MORE POINTS????
-        cin.ignore(1000, '\n');
-        repeat = want_continue();
-        //cout << "Enter to quit the program: ";
-        //cin.get();
-        //cout << "Goodbye..." << endl;
-
-        //contine?
-        //want_continue();
-        //repeat = want_continue();
+        cin.ignore(1000, '\n'); 
+        repeat = want_continue(); //call want_continue function to see if user wants to enter more points
     } 
-    assert(repeat == false);
+    assert(repeat == false); //if they don't want to repeat, then provide user the option to exit the program
     return 0; 
 } 

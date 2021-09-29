10d04bc996cad2a7d554bad8f5bb2889f5991d86
Mia Weber <miaweber02@gmail.com>
2021-09-29 13:28:28 -0600

    kattis

---
+++ kattis/hello/hello.cpp
@@ -0,0 +1,39 @@
+/* 
+    Mia Weber
+    29/Sept/2021
+    Hello World --> Kattis
+*/
+
+#include <iostream>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+void test();
+string answer();
+void solve();
+
+int main(int argc, char* argv[]) {
+    cout << argc << argv[1] << endl;
+    if (argc > 1 && string(argv[1]) == "test")
+        test();
+    else 
+        solve();
+    return 0;
+}
+
+string answer() {
+    return "Hello World!";
+}
+
+void test() {
+    assert(answer() == "Hello World!");
+    cerr << "All test cases passed...\n";
+}
+
+void solve() {
+    cout << answer() << endl;
+}
+
+

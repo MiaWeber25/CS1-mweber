5f39abf3cdae8f753e9fac02c8f9e0a64c8943b5
Mia Weber <miaweber02@gmail.com>
2021-09-30 15:02:16 -0600

    kattis problems work

--- kattis/jack-o-lantern/jack-o-lantern.cpp
+++ kattis/jack-o-lantern/jack-o-lantern.cpp
@@ -7,38 +7,38 @@
 #include <iostream>
 #include <string>
 #include <cassert>
-#include <math.h> 
+#include <math.h>
 
-using namespace std; 
+using namespace std;
 
 void test();
-int answer();
+int answer(int r1, int s);
 void solve();
 
 int main(int argc, char* argv[]) {
     if (argc > 1 && string(argv[1]) == "test")
         test();
     else 
-        answer();
+        solve();
     return 0;
 }
 
-int answer(int N, int T, int M) {
-    char ch;
-    cin >> N >> ch >> T >> ch >> M >> ch;
-    int ans = (N+T+M);
-    cout << ans;
-    return 0;
+int answer(int n, int t, int m) {
+//s*2-R1
+return (n*t*m); 
 }
 
 void test() {
-    //assert(answer() == );
-    //cerr << "All test cases passed...\n";
+    assert(answer(500,25,2) == 25000);
+    assert(answer(4,3,1) == 12);
+    assert(answer(250,42,12) == 126000);
+    cerr << "All test cases passed...\n";
 }
 
 void solve() {
-    //cout << answer() << endl;
+    int n;
+    int t;
+    int m;
+    cin >> n >> t >> m;
+    cout << answer(n,t,m) << endl;
 }
-
-
-

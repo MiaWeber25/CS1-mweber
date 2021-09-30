ca45874b136732fd05add8d4935d677d19af78de
Mia Weber <miaweber02@gmail.com>
2021-09-30 13:02:18 -0600

    kattis r2

---
+++ kattis/jack-o-lantern/jack-o-lantern.cpp
@@ -0,0 +1,44 @@
+/*
+    Mia Weber
+    29/Sept/2021
+    Jack-O'-Lantern Juxtaposition --> Kattis
+*/
+
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <math.h> 
+
+using namespace std;
+
+void test();
+int answer();
+void solve();
+
+int main(int argc, char* argv[]) {
+    if (argc > 1 && string(argv[1]) == "test")
+        test();
+    else 
+        answer();
+    return 0;
+}
+
+int answer(int N, int T, int M) {
+    char ch;
+    cin >> N >> ch >> T >> ch >> M >> ch;
+    int ans = (N+T+M);
+    cout << ans;
+    return 0;
+}
+
+void test() {
+    //assert(answer() == );
+    //cerr << "All test cases passed...\n";
+}
+
+void solve() {
+    //cout << answer() << endl;
+}
+
+
+

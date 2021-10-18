8517df518945fc28e41bc764aa708a5a9c3294de
Mia Weber <miaweber02@gmail.com>
2021-10-17 20:26:10 -0600

    loops lab

--- labs/loops/main.cpp
+++ labs/loops/main.cpp
@@ -17,7 +17,7 @@ using namespace std;
 void printTriangle(int);
 void printFlippedTriangle(int);
 void clearScreen();
-int main(int, char);
+int main(int, char*[]);
 
 //function takes height as an argument to print the triangle of that height with *
 void printTriangle(int height) {
@@ -36,11 +36,24 @@ void printTriangle(int height) {
 //implement the function that takes height as an argument and prints a triangle with * of given height
 void printFlippedTriangle(int height) {
     //FIXME3...
+    
+
 }
 
 //FIXME4...
 //design and implement a function that takes an integer as height and prints a square of the given height wiht *
-
+void printSquare(int height) {
+    int row = 1;
+    //row
+    while (row <= height) {
+        //column
+        for (int col = 0; col <= height; col++) {
+            cout << "* ";
+        }
+        row +=1;
+        cout << endl;
+    }
+}
 //function clears the screen system call
 //NOTE: system call is not a security best practice!
 void clearScreen() {
@@ -56,7 +69,8 @@ int main(int argc, char* argv[]) {
     //fIXME5... add a loop to make the program continue to run until the user wants to quit
     
     //FIXME6... call clearScreen function to clear screen for each round of the loop
-    
+    clearScreen();
+
     int height;
     cout << "Program prints geometric shapes of given height with *\n";
     cout << "Please enter the height of the shape: ";
@@ -65,9 +79,10 @@ int main(int argc, char* argv[]) {
     printTriangle(height);
 
     //fIXME7... call printFlippedTriangle passing proper argument & manually test the function
+    printFlippedTriangle(height);
 
     //FIXME8... call the function defined in FIXME4 passing proper argument & manually test the function
-
+    printSquare(height);
     //FIXME9... prompt user to enter y/Y to continue anything else to quit
 
     //FIXME10...use conditional statements to break the loop or continue the loop

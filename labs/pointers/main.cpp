9b90bff2e8123a514bd55d67f332450e5fd2e801
Mia Weber <miaweber02@gmail.com>
2021-10-21 19:17:18 -0600

    progress on pointers lab

---
+++ labs/pointers/main.cpp
@@ -0,0 +1,167 @@
+/*
+  Lab 6: Pointers, enum type, and namespace Lab
+  Updated By: Mia Weber
+  Date: 19/Oct/2021
+  Program demonstrates the use of preprocessor directive, namespace, enum type, and Pointers
+*/
+
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+//macro function
+#ifdef _WIN32
+#define clear() system("cls")
+#else
+#define clear() system("clear")
+#endif
+
+//user-defined enumerated type OPERATION
+enum OPERATION { ADD, MULTIPLY, SUBTRACT, LARGER, QUIT };
+
+//C++ way of defining type alias
+using big_int = long long int;
+
+//C way of defining type alias
+typedef long long int large_int;
+//both large_int and big_int are alias to long long int type
+
+//declare MyFunctions namespace
+namespace MyFunctions {
+  //function prototypes defined within the namespace
+  big_int findSum(const big_int *, const big_int *);
+  big_int findLarger(const big_int *, const big_int *);
+  large_int findProduct(const large_int &, const large_int &);
+  large_int findDifference(const large_int &, const large_int &);
+}
+
+void showMenu(void);
+OPERATION getOperation(char);
+
+int main(int argc, char* argv[]) {
+  char input;
+  big_int *num1; // a pointer that points to unsigned int --> big_int data type
+  num1 = new big_int; //allocate memory in heap and store the address in num1
+  big_int *num2 = new big_int; //declare num2 as a dynamic variable
+  large_int *sum = new large_int; //declare sum as a dynamic variable
+  large_int *prod = new large_int;
+  //FIXME3 - DECLARE DYNAMIC VARIABLES TO STORE DIFFERENCE AND LARGER VALUES ##FIXED##
+  large_int *diff = new large_int;
+  big_int *larger = new big_int;
+  //Must use these variables to store the returned values from MyFunctions
+
+  OPERATION oper;
+
+  //FIXME4: ADD Do WHILE LOOP TO CONTINUE THE PROGRAM UNTIL THE USER WANTS TO QUIT
+  //FIXME5: CALL CLEAR FUNCTION DEFINED ABOVE TO CLEAR THE SCREEN
+
+  showMenu();
+  cin >> input;
+  oper = getOperation(input);
+  switch (oper) {
+    case ADD:
+      cout << "Enter two whole numbers seperated by space: ";
+      //store the date by dereferencing Pointers
+      cin >> *num1 >> *num2;
+      //passing pointers to findSum FUNCTION
+      *sum = MyFunctions::findSum(num1, num2);
+      printf("%lld + %lld = %lld\n", *num1, *num2, *sum);
+      break;
+      //FIXME6: COMPLETE THE REST OF THE CASES TO PERFORM OTHER OPERATIONS ##FIXED##
+    case MULTIPLY:
+      cout << "Enter two whole numbers seperated by space: ";
+      //store the data by deferencing Pointers
+      //cin.ignore(1000,'\n');
+      cin >> *num1 >> *num2;
+      //passing pointers to findSum FUNCTION
+      *prod = MyFunctions::findProduct(*num1, *num2);
+      printf("%lld * %lld = %lld\n", *num1, *num2, *prod);
+      break;
+    case LARGER:
+      cout << "Enter two numbers seperated by space: ";
+      cin >> *num1 >> *num2;
+      *larger = MyFunctions::findLarger(num1, num2);
+      if (*larger == *num1)
+        printf("%lld > %lld\n", *larger, *num2);
+      else
+        printf("%lld > %lld\n", *larger, *num1);
+      break;
+    case SUBTRACT:
+      cout << "Enter two numbers seperated by space: ";
+      cin >> *num1 >> *num2;
+      *diff = MyFunctions::findDifference(*num1, *num2);
+      printf("%lld - %lld = %lld\n", *num1, *num2, *diff);
+      break;
+    default:
+      break;
+  }
+
+  delete num1;
+  delete num2;
+  delete sum;
+  delete prod;
+  cin.ignore(1000, '\n');
+  cout << "Good bye! Enter to exit the program...";
+  cin.get();
+  return 0;
+}
+
+void showMenu(void) {
+  cout << "Enter one of the following menu options:\n";
+  cout << "[1] Add two integers\n";
+  cout << "[2] Multiply two integers\n";
+  cout << "[3] Subtract one integer from another\n";
+  cout << "[4] Find the larger of two integers\n";
+  cout << "[5] Quit the program\n";
+  cout << "Enter your choice [1-4]: ";
+}
+
+//function returns OPERATION type given character choice
+OPERATION getOperation(char choice) {
+  OPERATION op;
+  switch(choice) {
+    case '1':
+      op = ADD;
+      break;
+    case '2':
+      op = MULTIPLY;
+      break;
+    case '3':
+      op = SUBTRACT;
+      break;
+    //FIXME7: ADD CASE FOR LARGER ##FIXED##
+    case '4':
+      op = LARGER;
+      break;
+    default:
+      op = QUIT;
+  }
+  return op;
+}
+
+//must use MyFunctions namespace to resolve findSum Function
+big_int MyFunctions::findSum(const big_int *n1, const big_int *n2) {
+  //dereference pointers n1 and n2 before adding their values
+  return (*n1) + (*n2);
+}
+
+//FIXME8: DEFINE FINDLARGER FUNCTION DECLARED INSIDE MYFUNCTIONS NAMESPACE ##FIXED##
+//function returns the larger of the two given values
+big_int MyFunctions::findLarger(const big_int *n1, const big_int *n2) {
+  if (*n2 > *n1) {
+    return *n2;
+  } else {
+    return *n1;
+  }
+}
+
+large_int MyFunctions::findProduct(const large_int &n1, const large_int &n2) {
+  return n1 * n2;
+}
+
+//FIXME9: DEFINE FINDDIFFERENCE FUNCTION DECLARED INSIDE MYFUNCTIONS NAMESPACE ##FIXED##
+//return the value of second big_int subtracted from the first
+large_int MyFunctions::findDifference(const large_int &n1, const large_int &n2) {
+  return n1 - n2;
+}

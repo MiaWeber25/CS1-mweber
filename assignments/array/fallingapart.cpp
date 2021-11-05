2950201f624e92925bb34e0c1e7b75595ea77378
Mia Weber <miaweber02@gmail.com>
2021-11-05 12:16:52 -0600

    updated fallingapart.cpp

---
+++ assignments/array/fallingapart.cpp
@@ -0,0 +1,78 @@
+/*
+    Created By: Mia Weber
+    Date: 03/Nov/2021
+    Falling Apart - Kattis Problem located HERE: https://open.kattis.com/problems/fallingapart 
+
+    Program does _____
+    ALGORITHM STEPS:
+    1.
+    2.
+    3.
+*/
+
+//use function[s] design your solution in such a way that it can be tested independently using assert statements
+//write a test function that automatically tests (using assert functions) three different input valeus other than the ones provided by the problem
+    // test must be called when an argument "test" is passed to main
+//screenshot of code, testing, and kattis accepted submission
+//update README
+
+#include <string>
+#include <algorithm>
+#include <iterator> //DO I NEED THIS ONE? --> begin() and end()
+#include <iostream> 
+#include <cassert>
+#include <math.h>
+
+
+using namespace std;
+
+//FUNCITON PROTOTYPE
+void test();
+//int answer(int * pile[], int size);
+void solve();
+
+
+int main(int argc, char* argv[]) {
+    if (argc > 1 && string(argv[1]) == "test")
+        test();
+    else 
+        solve();
+    return 0;
+}
+
+int answer(int size, int pile[]) {
+    //sort the array
+    //alternate for players turns (variable and flag)
+        //store element in correct player_sum variable and then change element to 0
+    //stop iterating when all elements in the array are = 0
+    //find and return the larger player_sum variable
+    //**this just prints array for testing purposes** 
+    for (int j=0; j<size; j++) {
+        cout << pile[j];
+    }
+}
+
+void test() {
+    //I NEED THREE TESTS + KATTIS TESTS
+    //assert(answer(___, ___) == _____);
+    //assert(answer(___, ___) == _____);
+    //assert(answer(___, ___) == _____);
+    cerr << "All test cases passed...\n";
+}
+
+void solve() {
+    int size;
+    //create a new dynamic array to store the values of the integers
+    int * pile = new int[size]; //REMEMBER TO DELETE THIS STACK MEMORY!!!
+    cin >> size;
+    //read in the integers from the command line and store them in the array pile
+    for (int i=0; i< size; i++) {
+        cin >> pile[i];
+    }
+    //**this just prints array for testing purposes** 
+    //for (int j=0; j<size; j++) {
+     //   cout << pile[j] << endl;
+    //}
+    answer(size, pile);
+    //cout << answer(size, & pile) << endl;
+}

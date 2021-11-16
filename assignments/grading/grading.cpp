b65dea775976bb00f3860d36d2628a703b25fb67
Mia Weber <miaweber02@gmail.com>
2021-11-16 13:27:49 -0700

    HW 8 updates and changes

--- assignments/grading/grading.cpp
+++ assignments/grading/grading.cpp
@@ -9,7 +9,7 @@
     1. Prompt the user to enter an input file name to read data from ***DONE!!***
     2. Read n number of student records from input file into an array ***DONE!!***
     3. Prompt the user to enter an outfile name to write the data to ***DONE!!***
-    4. Write functions to find student's average test scores and the letter grade using a standard grading scale
+    4. Write functions to find student's average test scores and the letter grade using a standard grading scale ***DONE!!***
     5. Write a function to sort the array of student records in descending order based on avg test scores
     **BONUS
 */
@@ -74,7 +74,10 @@ void writeData(vector<studentData> & gradebook) {
     getline(cin, outFile);
     ofstream fout;
     fout.open(outFile);
-    fout << "input from input file: " << endl;
+    //FOR TESTING: fout << "input from input file: " << endl;
+    //=== line
+    fout << setw(60) << setfill('=');
+    fout << endl << "fname" << setw(5) << " " << "lname" << setw(5) << " " << endl;
     for (int i=0; i<gradebook.size(); i++) {
         fout << gradebook[i].firstName << endl;
         fout << gradebook[i].lastName <<endl;
@@ -83,10 +86,9 @@ void writeData(vector<studentData> & gradebook) {
         }
     }
     fout << "average: " << findAvg(gradebook) << endl;
+    fout << "letter grade: " << findLetterGrade(gradebook) << endl;
 }
 
-
-//float findAvg(vector<studentData> &); //WILL THIS TAKE A VECTOR OR A STRUCT?
 //function to find the average of the test scores
 float findAvg(vector<studentData> & gradebook) {
     float sum;
@@ -97,3 +99,33 @@ float findAvg(vector<studentData> & gradebook) {
     }
     return sum/4;
 }
+
+//function to find the letter grade that corresponds to the average test grade
+char findLetterGrade(vector<studentData> & gradebook) {
+    if (findAvg(gradebook) >= 90)
+        return 'A';
+    else if (findAvg(gradebook) >= 80)
+        return 'B';
+    else if (findAvg(gradebook) >= 70)
+        return 'C';
+    else if (findAvg(gradebook) >= 60)
+        return 'D';
+    else 
+        return 'F';
+}
+
+//function to sort the array in descending order based off average test scores
+int sortArray(vector<studentData> & gradebook) {
+   for (int i=0; i<gradebook.size(); i++) {
+       for (int j=0; j<4; j++) {
+           //sort(gradebook.begin(), gradebook.end(), gradebook.tests[j]);
+       }
+   }
+    return 0;
+}
+
+
+//TO DO: 
+//  1. format output
+//  2. sort array - and print in sorted order
+//  3. find bonus info - class avg, class max, class min, total a,b,c,d,f

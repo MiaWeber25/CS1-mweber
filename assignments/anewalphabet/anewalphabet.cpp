d439bdaa15ce82e12ac5223f0c8d4db1ae4ccf28
Mia Weber <miaweber02@gmail.com>
2021-11-30 18:49:51 -0700

    updates to anewalphabet

--- assignments/anewalphabet/anewalphabet.cpp
+++ assignments/anewalphabet/anewalphabet.cpp
@@ -1,7 +1,7 @@
 /*
     Mia Weber
     30/Nov/2021
-    A New Alphabet 
+    A New Alphabet Kattis Problem Here: https://open.kattis.com/problems/anewalphabet 
 */
 
 #include <iostream>
@@ -51,48 +51,47 @@ void solve() {
     cypher['s'] = "$";
     cypher['t'] = "']['";
     cypher['u'] = "|_|";
-    cypher['v'] = "\\//"; //SEE IF THIS WORKS...
-    cypher['w'] = "\\//\\//"; //SEE IF THIS WORKS...
+    cypher['v'] = "\\/"; //SEE IF THIS WORKS...
+    cypher['w'] = "\\/\\/"; //SEE IF THIS WORKS...
     cypher['x'] = "}{";
-    cypher['y'] = "`//";
+    cypher['y'] = "`/";
     cypher['z'] = "2";
 
     string input;
-    //cin.ignore(' ');
     getline(cin, input);
-    cout << "This is the input: " << input << endl; //FOR TESTING PURPOSES - DELETE LATER
+    //cout << "This is the input: " << input << endl; //FOR TESTING PURPOSES - DELETE LATER
 
     vector<char> plainText(input.begin(), input.end());
-    vector<string> cypherText;
-    //FOR LOOP FOR TESTING VECTOR METHOD - DELETE LATER
-   // for(int i = 0; i<30; i++) {
-       // cout << plainText[i] << endl;
-        //determine if the plain text char is in cypher map 
-       // if (cypher.count(plainText[i])==1) { //if it is present, add that value to cypherText vector
-            //found! 
-            //cypherText.push_back(cypher.second); 
+    //vector<string> cypherText;
+    string cypherText;
 
-      //  }
-  //  }
-    
     for (char i: plainText) { //iterate over the plainText 
-        cout << i << " "; //i is the individual char in plainText
-        tolower(plainText[i]);
-        for (auto e: cypher) { //iterate over the cypher map
-            if(e.first == i) //it the char in plainText matches the first element in map
+        //tolower(plainText[i]);
+        //cout << tolower(i) << " "; //i is the individual char in plainText
+       /* for (auto e: cypher) { //iterate over the cypher map
+            if(e.first == i) //is it plainText[i] or just i
                 cypherText.push_back(e.second); //add the value to final vector
-                //cout << e.second;
             else {
-                //cout << "NO";
                 //string(plainText[i]);
                 //cypherText.push_back(plainText[i]); //otherwise, add the origional value to the final vector
                 cypherText.push_back("*");
             }
+        }*/
+        auto it = cypher.find(i);
+        if (it != cypher.end()) {
+            //cypherText.push_back(it->second); //WORKED FOR THE VECTOR
+            cypherText+=it->second;
         }
+        else {
+            //string(i);
+            cypherText.push_back(i);
+        }
+       // i++; DO I NEED THIS? I DON'T THINK THAT I DO...
     } 
     for (auto m: cypherText) {
         cout << m << " ";
     }
+    cout << endl;
 }
 
 
